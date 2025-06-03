c7a2c60d127fdb10cf84c2cc8e15da61febb3649
Caruso-08 <matheus.pereira.santos19@escola.pr.gov.br>
2025-06-02 21:05:42 -0300

    Atualizar o exercicio4.py

--- capitulo2/exercicio4.py
+++ capitulo2/exercicio4.py
@@ -1,7 +1,9 @@
-populacao_atual = 8231613070  
-taxa_crescimento = 0.0085     
+populacaoMundial = float(input("digite o numero da população mundial "))
+crescimentoPopulacional = float(input("digite a taxa (%) ")) / 100
+ano = int(input("Digite o número de anospara estimar o crescimento: "))
+populacaoFutura = populacaoMundial * (1 + crescimentoPopulacional) * ano
 
-print("Estimativa da População Mundial:")
-for ano in range(1, 6):
-    populacao_futura = populacao_atual * ((1 + taxa_crescimento) ** ano)
-    print(f"Ano {ano}: {int(populacao_futura):,} pessoas")
+
+
+
+print("A população estimada após {ano} ano{s} será de:", {populacaoFutura})
