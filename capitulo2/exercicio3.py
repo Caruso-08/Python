0973a77ed9db5141e5f8cda570390a17377795fd
Caruso-08 <matheus.pereira.santos19@escola.pr.gov.br>
2025-06-02 21:01:47 -0300

    Atualizar o exercicio3.py

--- capitulo2/exercicio3.py
+++ capitulo2/exercicio3.py
@@ -1,43 +1,11 @@
-print("Tempo do primeiro trecho: 8 min e 15 s")
-minutoPrimeiroTrecho = 8 * 60
-segundoPrimeiroTrecho = 15
+raio = int(input("Digite o raio do círculo: ")
 
-print("Tempo do segundo trecho: 7min e 12seg")
-minutoSegundoTrecho = (7 * 3) * 60
-segundosSegundoTrecho = 12 * 3
+pi = 3,14159
 
-print("Tempo do terceiro trecho: 8 min e 15 s")
-minutoTerceiroTrecho = 8 * 60
-segundoTerceiroTrecho = 15
+diâmetro = 2 * raio 
+circunferência = 2 * pi * raio 
+área = pi * raio * raio
 
-# Soma o total de minutos e converte todos os segundos em minutos
-totalTempoTodosTrechosMinutos = (minutoPrimeiroTrecho +
-                                 minutoSegundoTrecho +
-                                 minutoTerceiroTrecho) / 60
-
-# Soma valor total dos segundos
-totalTempoTodosTrechosSegundos = (segundoPrimeiroTrecho +
-                                  segundosSegundoTrecho +
-                                  segundoTerceiroTrecho)
-
-restanteMinutos = int(totalTempoTodosTrechosSegundos / 60)
-restanteSegundos = totalTempoTodosTrechosSegundos % 60
-
-totalMinutos = totalTempoTodosTrechosMinutos + restanteMinutos
-totalSegundos = restanteSegundos
-
-print("Soma de tempo de todos os trechos: ", totalMinutos,
-      "minutos")
-print("Soma de tempo de todos os trechos: ", totalSegundos,
-      "segundos")
-
-horaInicialSegundos = (6 * 60) * 60
-minutoInicialSegundos = 52 * 60
-horarioInicialSegundos = horaInicialSegundos + minutoInicialSegundos
-
-tempoTrechoMinutosSegundos = totalMinutos * 60
-
-horaChegada = int(((horarioInicialSegundos + tempoTrechoMinutosSegundos) / 60) / 60)
-minutoChegada = int(((minutoInicialSegundos + tempoTrechoMinutosSegundos) / 60) % 60)
-
-print("O Tempo de chegada foi de ", horaChegada,":", minutoChegada, restanteSegundos)
+print(f"Diâmetro: {diâmetro}")
+print(f"Circunferência: {circunferência}")
+print(f"Área: {área}")
