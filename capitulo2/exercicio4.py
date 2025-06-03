6b56b89bb0933b71cb2ff5d50cbc6e3d6eb3520b
Caruso-08 <matheus.pereira.santos19@escola.pr.gov.br>
2025-06-02 21:03:41 -0300

    Atualizar o exercicio4.py

--- capitulo2/exercicio4.py
+++ capitulo2/exercicio4.py
@@ -1,8 +1,6 @@
-# Dados iniciais
-populacao_atual = 8231613070  # População em 2025
-taxa_crescimento = 0.0085     # 0,85% ao ano
+populacao_atual = 8231613070  
+taxa_crescimento = 0.0085     
 
-# Cálculo e exibição das projeções para os próximos 5 anos
 print("Estimativa da População Mundial:")
 for ano in range(1, 6):
     populacao_futura = populacao_atual * ((1 + taxa_crescimento) ** ano)
