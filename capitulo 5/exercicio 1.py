d203cefb474b539047b79c4dec2d40f3dd0d90a0
VITORIA DOS SANTOS MARCELINO <vitoria.santos.marcelino@escola.pr.gov.br>
2025-09-23 11:52:33 -0300

    exercicio

---
+++ capitulo 5/exercicio 1.py
@@ -0,0 +1,10 @@
+def apresentacao(nome, nomeMae, nomePai):
+    print(f"Nome: {nome}")
+    print(f"Nome da Mae: {nomeMae}")
+    print(f"Nome do Pai: {nomePai}")
+
+nome = str(input("Digite seu nome completo"))
+nomeMae = str(input("Digite o nome completo da sua mãe"))
+nomePai = str(input("Digite o nome completo do seu pai"))
+
+apresentacao(nome, nomeMae, nomePai)
